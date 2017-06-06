526d8f1f3df263525345307e8faae4224afe624c
jyaaan <john.yamashiro@gmail.com>
2017-06-06 07:43:34 -0700

    adding automated login to eatifyjohn #1

--- selenium-test.js
+++ selenium-test.js
@@ -6,4 +6,26 @@ var driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
-driver.get('https://www.instagram.com/accounts/login/');
+
+
+function login() {
+    driver.get('https://www.instagram.com/accounts/login/')
+        .then(browse => {
+            console.log('browse:', browse);
+        })
+    driver.findElement(By.className('_kp5f7 _qy55y'))
+        .sendKeys('eatifyjohn')
+        .then(result => {
+            driver.findElement(By.className('_kp5f7 _1mdqd _qy55y'))
+                .sendKeys('occsbootcamp');
+        })
+        .then(second => {
+            console.log('second:', second);
+            // setTimeout(() => {
+                driver.findElement(By.className('_ah57t _84y62 _i46jh _rmr7s'))
+                    .click();
+            // }, 2000);
+        })
+}
+
+login();
