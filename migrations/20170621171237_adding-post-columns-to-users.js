44bc30c782f7fa727a81cea3fc2aa1a0e5011aa6
jyaaan <john.yamashiro@gmail.com>
2017-06-21 17:49:18 -0700

    changing migration file to remove duplicate #42

--- migrations/20170621171237_adding-post-columns-to-users.js
+++ migrations/20170621171237_adding-post-columns-to-users.js
@@ -1,8 +1,6 @@
 
 exports.up = function(knex, Promise) {
   const query = knex.schema.table('users', table => {
-    table.integer('recent_like_count').defaultTo(null);
-    table.integer('recent_comment_count').defaultTo(null);
     table.integer('recent_post_count').defaultTo(null);
     table.integer('recent_post_duration').defaultTo(null);
   })
@@ -12,8 +10,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   const query = knex.schema.table('users', table => {
-    table.dropColumn('recent_like_count');
-    table.dropColumn('recent_comment_count');
     table.dropColumn('recent_post_count');
     table.dropColumn('recent_post_duration');
   });
