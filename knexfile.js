14884f5a6cba234e5fbe51cc5b24e33f40ce8f6e
jyamashiro <jyamashiro@linkedin.com>
2017-09-07 13:23:36 -0700

    adding environment variables to database config

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
 exports.development = {
   client: 'postgresql',
   connection: {
-    user: 'johny',
-    password: 'peanut',
-    database: 'eatify-crm'
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    port: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME
   },
   migrations: {
     directory: './migrations',
