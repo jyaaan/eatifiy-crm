d874694178b1fc946254cb22a1b6f9cf93114ccc
jyamashiro <jyamashiro@linkedin.com>
2017-11-16 11:34:08 -0800

    adding debug options #107

--- server/messaging.js
+++ server/messaging.js
@@ -2,6 +2,7 @@ const twilio = require('twilio');
 
 class Messaging {
   constructor() {
+    console.log('env vars: ' + process.env.TWILIO_ACCOUNT_SID + ' , ' + process.env.TWILIO_AUTH_TOKEN)
     this.twilioClient = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
     this.sentMessageCount = 0;
   }
