078e22999ce284119196427c7c0c15a8f3ff060f
jyamashiro <jyamashiro@linkedin.com>
2017-11-16 11:44:10 -0800

    removing twilio #107

--- server/messaging.js
+++ server/messaging.js
@@ -3,17 +3,18 @@ const twilio = require('twilio');
 class Messaging {
   constructor() {
     console.log('env vars: ' + process.env.TWILIO_ACCOUNT_SID + ' , ' + process.env.TWILIO_AUTH_TOKEN)
-    this.twilioClient = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    this.sentMessageCount = 0;
+    // this.twilioClient = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    // this.sentMessageCount = 0;
   }
   
   send(message) {
-    this.twilioClient.messages.create({
-      to: process.env.MY_NUM,
-      from: process.env.TWILIO_NUM,
-      body: message
-    });
-    this.sentMessageCount++;
+    // this.twilioClient.messages.create({
+    //   to: process.env.MY_NUM,
+    //   from: process.env.TWILIO_NUM,
+    //   body: message
+    // });
+    // this.sentMessageCount++;
+    console.log(message);
   }
 }
 
